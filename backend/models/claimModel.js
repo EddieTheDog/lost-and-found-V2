9fc2d6c92ede76dc634a2287128bf00927ed9a3c
EddieTheDog <1dylanroeder1@gmail.com>
2025-11-02 10:59:17 -0800

    Create claimModel.js

---
+++ backend/models/claimModel.js
@@ -0,0 +1,11 @@
+let claims = [];
+
+export const addClaim = async ({ name, email }) => {
+  const id = claims.length + 1;
+  const newClaim = { id, name, email, status: "open", items: [] };
+  claims.push(newClaim);
+  return id;
+};
+
+export const findClaimById = async (id) => claims.find(c => c.id == id);
+export const getClaims = async () => claims;
